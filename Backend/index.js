767f50e2a1a1438e8e459a80e582731890748e88
Matix97 <mateusz_baranski@outlook.com>
2019-11-01 23:15:28 +0100

    c*** ile mozna

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,24 @@
 'use strict';
 var Hapi = require('hapi');
+const MySQL = require('mysql');
 
 const server = new Hapi.Server({  
     host: 'localhost',
     port: 3000
   })
+
+  
+const connection = MySQL.createConnection({
+    connectionLimit : 100,
+    host: 'sql.teatr-fenixa.nazwa.pl',
+    user: 'teatr-fenix_PAW-DataBase',
+    password: '69696969xD',
+    database: 'teatr-fenix_PAW-DataBase',
+    //user: 'teatr-fenix_PKI',
+    //password: 'Qwerty12345',
+    //database:'teatr-fenix_PKI',
+    port: 3306
+});
 
 
 server.route({
@@ -12,6 +26,22 @@ server.route({
     path: '/',
     handler: function(request,reply){
         return('It is wokring');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/conect',
+    handler: function(request,reply){
+    
+        connection.connect(function(err) {
+          if (err) {
+            return console.error('error: ' + err.message+err.stack);
+          }         
+          console.log('Connected to the MySQL server.');
+        });
+        connection.query('CREATE TABLE [IF NOT EXISTS] Column(column_id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255) NOT NULL);')
+        connection.query('select table_schema as database_name, table_name from information_schema.tables');
+        return "Polaczylem sie lub nie xddd";
     }
 });
 

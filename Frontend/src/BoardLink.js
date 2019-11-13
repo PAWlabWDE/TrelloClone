2b7164a97b3af442096a6884904ef259c34dafd3
Leherguargin <maciejleszczynski1997@gmail.com>
2019-11-13 16:21:35 +0100

    linkowanie ominięte :)

--- Frontend/src/BoardLink.js
+++ Frontend/src/BoardLink.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import "./Todo.css";
 import "./App.css";
-const API = 'http://localhost:3001';
-
+const API = "http://localhost:3001";
 
 class BoardLink extends Component {
   constructor(props) {
@@ -13,24 +12,20 @@ class BoardLink extends Component {
     this.props = {
       name: ""
     };
-
   }
   handleSubmit(e) {
     e.preventDefault();
-   
-      fetch(API + "/chooseBoard", {           
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          boardName: this.state.text
-        })
+
+    fetch(API + "/chooseBoard", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        boardName: this.state.text
       })
-    
-
-
+    });
   }
   render() {
     var linkAdddres = "/".concat(this.props.name);
@@ -38,12 +33,15 @@ class BoardLink extends Component {
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <div class="center">
-          <Link to={linkAdddres}><Button bsStyle="primary">{this.props.name}</Button></Link>
+        <div className="center">
+          {/* <Link to={linkAdddres}> */}
+          <a href={linkAdddres}>
+            <Button bsStyle="primary"> {this.props.name} </Button>
+          </a>
+          {/* </Link> */}
         </div>
-        <section class="section">{this.props.name}</section>
+        <section className="section"> {this.props.name} </section>
       </form>
-      
     );
   }
 }

1ef68b75889031d4fa5f79ce030ae3096db343e0
Matix97 <mateusz_barsnski@outlook.com>
2019-11-13 15:49:42 +0100

    próba naprawy warningów

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./BoardLink";
 import Todo from "./Todo";
 import "./Switcher";

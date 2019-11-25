1dca761396ec10c28d2dd55f2b2e0434e5c3757f
Matix97 <mateusz_barsnski@outlook.com>
2019-11-25 22:56:43 +0100

    nawet coś da się wstawić do abzki online

--- Backend/index.js
+++ Backend/index.js
@@ -36,19 +36,46 @@ const validate = async function (decoded, request, h) {
     }
 };
 
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const handlers = require('./handlers');
 
 const init = async () => {
-    Mongoose.connect('mongodb+srv://test:test@cluster0-8ecx4.azure.mongodb.net/test?retryWrites=true&w=majority', {
+    mongoose.connect('mongodb+srv://test:test@cluster0-8ecx4.azure.mongodb.net/test?retryWrites=true&w=majority');
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
 
-        useNewUrlParser: true
-    }).
-        catch(error => handleError(error));
-    Mongoose.connection.on('error', err => {
-        console.log("Cos jebalo");
-        logError(err);
+    db.once('open', function () {
+        console.log("Connection Successful!");
+
+        // define Schema
+        var BookSchema = mongoose.Schema({
+            name: String,
+            price: Number,
+            quantity: Number
+        });
+
+        // compile schema to model
+        var Book = mongoose.model('Book', BookSchema, 'bookstore');
+
+        // a document instance
+        var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
+
+        // save model to database
+        book1.save(function (err, book) {
+            if (err) return console.error(err);
+            console.log(book.name + " saved to bookstore collection.");
+        });
+
     });
+    // , {
+
+    //     useNewUrlParser: true
+    // }).
+    //     catch(error => handleError(error));
+    // Mongoose.connection.on('error', err => {
+    //     console.log("Cos jebalo");
+    //     logError(err);
+    // });
     const server = new Hapi.Server({
         port: port,
         routes: {

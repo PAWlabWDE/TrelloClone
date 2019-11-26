89090ac10cde6258c5565268d69d9b302cfd64af
Leherguargin <maciejleszczynski1997@gmail.com>
2019-11-27 00:27:50 +0100

    strona logowania

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./BoardLink";
 import Todo from "./Todo";
 import "./Switcher";
+import Loginn from "./logowanie";
 import Switcher from "./Switcher";
 
 class App extends Component {
-
   render() {
     return (
       <Router>
-        <container className="container" role="main">
-          <div className="row">
-            <header className="col-md-12 text-center text-white">
-              <h1> Trello clone </h1>
-            </header>
-            <nav className="col-md-3">
-              <Todo />
-            </nav>
-            <section className="col-md-9">
-              <Switcher />
-            </section>
-          </div>
-        </container>
+        <Route exact path="/log">
+          <Loginn />
+        </Route>
+        <Route exact path="/">
+          <container className="container" role="main">
+            <div className="row">
+              <header className="col-md-12 text-center text-white">
+                <h1> Trello clone </h1>
+              </header>
+              <nav className="col-md-3">
+                <Todo />
+              </nav>
+              <section className="col-md-9">
+                <Switcher />
+              </section>
+            </div>
+          </container>
+        </Route>
       </Router>
     );
   }
--- Frontend/src/logowanie.js
+++ Frontend/src/logowanie.js
@@ -1,5 +1,48 @@
-export default class App extends Component {
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export default class Loginn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "", password: "" };
+    this.emailWritten = this.emailWritten.bind(this);
+    this.passwordWritten = this.passwordWritten.bind(this);
+  }
+  emailWritten(e) {
+    this.setState({ email: e.target.value });
+  }
+  passwordWritten(e) {
+    this.setState({ password: e.target.value });
+  }
   render() {
-    return <div> </div>;
+    return (
+      <div className="row">
+        <div className="col-md-4 mt-5"></div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="text-white"> Email address </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={this.emailWritten}
+            />
+            <Form.Text className="text-muted">
+              We 'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label className="text-white"> Password </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.passwordWritten}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Zaloguj
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }

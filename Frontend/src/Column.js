7890dab694e40b2336a6e463ce496b56efdef0b1
Matix97 <mateusz_barsnski@outlook.com>
2019-12-15 19:49:47 +0100

    działa w sumie wszytko poza dnd, ale ucina początek karty...

--- Frontend/src/Column.js
+++ Frontend/src/Column.js
@@ -5,18 +5,21 @@ import { Cell as ColTable } from "react-sticky-table";
 import { Button } from "react-bootstrap";
 import Popup from "reactjs-popup";
 import Cookie from "js-cookie";
+import TextEllipsis from 'react-text-ellipsis';
+import { Container, Row, Col } from "react-bootstrap";
 
 const API = "http://localhost:3001";
 const ADD_COLUMN_QUERY = "/addCard";
 
-export default class Column extends Component{
+
+export default class Column extends Component {
   constructor(props) {
     super(props);
     this.state = {
       textFieldValue: "",
       boardName: props.boardName,
       tasks: props.tasks,
-      name:props.name      
+      name: props.name
     };
 
     this.addCardHandler = this.addCardHandler.bind(this);
@@ -27,31 +30,33 @@ export default class Column extends Component{
     console.log(
       "dodaje nową kartę do tablicy: " +
       this.state.boardName +
-        " do kolumny: " +
-     this.state.name +
-     " nazawa zadania: "+
-     this.state.textFieldValue
+      " do kolumny: " +
+      this.state.name +
+      " nazawa zadania: " +
+      this.state.textFieldValue
 
     );
-   if(this.state.textFieldValue!==""){ 
-    fetch(API + ADD_COLUMN_QUERY + "?token=" + Cookie.get("token"), {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        boardName: this.props.boardName,
-        columnName: this.state.name,
-        newTask: this.state.textFieldValue
-      })
-    });
-  }}
+    if (this.state.textFieldValue !== "") {
+      fetch(API + ADD_COLUMN_QUERY + "?token=" + Cookie.get("token"), {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          boardName: this.props.boardName,
+          columnName: this.state.name,
+          newTask: this.state.textFieldValue
+        })
+      });
+    }
+  }
   handleChange(event) {
     this.setState({ textFieldValue: event.target.value });
   }
-  render(){
-  
+
+  render() {
+
     return (
       <ColTable>
         <div className="lista" >
@@ -60,35 +65,59 @@ export default class Column extends Component{
           </div>
           {this.state.tasks.map((item, index) => {
             return (
-              <div className="karta">
-                <div className="p">
-                  <div class="text-center text-white">
-                    <Popup
-                      trigger={
-                        <Button variant="success"> {item["nazwaZadania"]}</Button>
+
+              <Popup
+                trigger={
+                  <div className="karta" onClick={e => console.log("FUCK YOU")}>
+
+                    <Row>
+                      {item["label"].map((item, index) => {
+                        return (
+                          <Row  >
+                            <a style={{ backgroundColor: item.labelColor, "width": "50px", "height": "20px"}}> </a>
+                            <a style={{ backgroundColor: "#808080", "width": "50px", "height": "20px"}}> </a>
+                          </Row>
+                        );
+                      })
                       }
-                      position="top center"
-                      closeOnDocumentClick
-                    >
-                      <Card taskName={item["nazwaZadania"]} boardName={this.state.boardName} columnName={this.state.name} taskComment={item.komentarze} attachments={item.zalaczniki} labels={item.label} history={item.history} taskID={item.nrZadania} />
-                    </Popup>
+                    </Row>
+                    <Row>
+
+
+                      <div className="display-linebreak" class=" text-white">
+                        {item["nazwaZadania"].split("\n").map((i, key) => {
+                        //  console.log("Line "+i);
+                          return <div key={key}>{i}</div>;
+                        })}
+                      </div>
+
+
+
+                    </Row>
+
                   </div>
-                </div>
-              </div>
+                }
+                position="top center"
+                closeOnDocumentClick
+              >
+                <Card taskName={item["nazwaZadania"]} boardName={this.state.boardName} columnName={this.state.name} taskComment={item.komentarze} attachments={item.zalaczniki} labels={item.label} history={item.history} taskID={item.nrZadania} />
+              </Popup>
+
             );
           })}
           <div>
-          <input
-                type="text"
-                value={this.state.textFieldValue}
-                onChange={this.handleChange}
-              />
+            <textarea
+              type="text"
+              value={this.state.textFieldValue}
+              onChange={this.handleChange}
+            />
             <Button variant="secondary" size="sm" onClick={this.addCardHandler}>
               New card
             </Button>
           </div>
         </div>
       </ColTable>
-    );}
+    );
+  }
 }
 
